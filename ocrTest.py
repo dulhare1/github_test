bf26102b4da25fa36ccff331521bedeb34a23632
dulhare1 <dulhare1+github@gmail.com>
2020-06-16 15:50:22 +0800

    參數帶入

--- ocrTest.py
+++ ocrTest.py
@@ -1,10 +1,11 @@
 import cv2
 import pytesseract
+import sys
+import json
 
 def cathayOCR(origineImage,resize_num=2,threshold=128,erodeIterationTimes=3,dilateIterationTimes=1,medianBlurKernel=3,isRevert=True):
 
     isShowImg = True
-
     # resize
     origineImage = cv2.resize(origineImage,(origineImage.shape[1] * int(resize_num), origineImage.shape[0] * int(resize_num)), interpolation=cv2.INTER_CUBIC)
 
@@ -37,10 +38,34 @@ def cathayOCR(origineImage,resize_num=2,threshold=128,erodeIterationTimes=3,dila
         cv2.imshow('resultImg', resultImg)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
+    pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR/tesseract.exe'
     return (pytesseract.image_to_string(resultImg, lang='eng', config='--psm 6 '))
 
 if __name__ == "__main__":
-    path = '/Users/moriakiraakira/Desktop/OCR_Sample/數字驗證碼1-2.png'
+
+    path= sys.argv[1]
+    json_str = sys.argv[2]
+
+    j = json.loads(json_str)
+    
+    # Set default value
+    # for key in j:
+    #     if j[key] is None:
+    #         del j[key]
+    #         continue
+    #     if j[key].isdigit():
+    #         int(j[key])
+
+
+
+    # path= 'data/test.PNG'
     origineImage = cv2.imread(path)
-    print(cathayOCR(origineImage))
+    print(j)
+
+    # cv2.imshow('oriImg', origineImage)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    
+    print(cathayOCR(origineImage, resize_num = None, threshold = int(j['threshold']), erodeIterationTimes = int(j['erodeIterationTimes']), \
+                    dilateIterationTimes = int(j['dilateIterationTimes']), medianBlurKernel = int(j['medianBlurKernel']), isRevert = j['isRevert']))
+    # print(cathayOCR(origineImage, **j))
